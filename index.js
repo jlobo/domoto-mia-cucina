19accdafcfee0ac784a338b9f73781389dd3e55d
José Lobo <jloboprs@gmail.com>
2016-06-10 17:51:55 -0500

    Se agrega el código de prueba

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+module.exports = new (class Index {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+	console.log('[-c°▥°]-c\t I\'m ready human!!!');
+  }
+
+  message() {
+  	return '[-c°▥°]-c\t Hello human!!!';
+  }
+})();
