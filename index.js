f80ee8b034e2ee7d2fe8907425e529f9f9751eea
José Lobo <jloboprs@gmail.com>
2016-06-28 17:32:25 -0500

    Agregar la vista a la extensión

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-#!/usr/bin/env node
+const path = require('path');
+const ImportTemplate = require('domoto/importTemplate');
+const ItemMenu = require('domoto/itemMenu');
+const EventEmitter = require('events');
 
-module.exports = new (class Index {
+module.exports = class installView extends EventEmitter {
   constructor() {
-    this.init();
+    super();
+
+    this.itemMenu = new ItemMenu('domoto-mia-cucina').setHeader('Mia cucina', {left: 'power_settings_new'});
+    this.body = new ImportTemplate(path.resolve(__dirname, './view.html'));
+    this.body.on('load', element => this._init(element));
   }
 
-  init() {
-	console.log('[-c°▥°]-c\t I\'m ready human!!!');
+  _init() {
+    this.emit('ready', this);
   }
-
-  message() {
-  	return '[-c°▥°]-c\t Hello human!!!';
-  }
-})();
+};
