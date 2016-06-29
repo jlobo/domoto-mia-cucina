9d4e245d0855bad883b6930eabad18af8bde993b
José Lobo <jloboprs@gmail.com>
2016-06-28 22:00:26 -0500

    Agregar el comportamiento a la vista para que modifique el estado de la estufa

--- index.js
+++ index.js
@@ -8,13 +8,44 @@ module.exports = class installView extends EventEmitter {
   constructor() {
     super();
 
+    this._time = null;
+    this._turnOn = null;
+    this._temperatures = null;
     this.client = new StoveClient();
-    this.itemMenu = new ItemMenu('domoto-mia-cucina').setHeader('Mia cucina', {left: 'power_settings_new'});
     this.body = new ImportTemplate(path.resolve(__dirname, './view.html'));
+    this.itemMenu = new ItemMenu('domoto-mia-cucina').setHeader('Mia cucina', {left: 'power_settings_new'});
+
     this.body.on('load', element => this._init(element));
   }
 
+  get temperature() {
+    return this._temperatures.find(temperature => temperature.checked).value;
+  }
+
+  get isOn() {
+    return this._turnOn.checked;
+  }
+
+  get time() {
+    return parseInt(this._time.value, 10);
+  }
+
+  turnOn() {
+    if (!this.isOn)
+      return this.client.turnOff();
+
+    const status = { minutes: this.time, temperature: this.temperature };
+    this.client.turnOn(status);
+  }
+
   _init() {
+    this._time = this.body.document.getElementById('tiempo');
+    this._turnOn = this.body.document.getElementById('encender');
+    this._temperatures = [this.body.document.getElementById('high'),
+      this.body.document.getElementById('medium'),
+      this.body.document.getElementById('low')];
+
+    this._turnOn.addEventListener('click', e => this.turnOn(e));
     this.emit('ready', this);
   }
 };
