1ae3d4cbf6b3e34407073d1a36a03021a05e4695
José Lobo <jloboprs@gmail.com>
2016-07-02 17:02:03 -0500

    La extensión debe heredar de Domoto

--- domotoMiaCucina.js
+++ domotoMiaCucina.js
@@ -1,26 +1,13 @@
-const ImportTemplate = require('domoto/importTemplate');
+const Domoto = require('domoto');
 const StoveController = require('./stoveController');
-const ItemMenu = require('domoto/itemMenu');
-const EventEmitter = require('events');
-const path = require('path');
+const templatePath = require('path').resolve(__dirname, './view.html');
 
-module.exports = class DomotoMiaCucina extends EventEmitter {
+module.exports = class DomotoMiaCucina extends Domoto {
   constructor() {
-    super();
-
-    this.controller = null;
-    this.name = 'domoto-mia-cucina';
-    this.itemMenu = new ItemMenu(this.name);
-    this.itemMenu.description = 'Mia cucina';
-    this.itemMenu.addLeftIcon('power_settings_new');
-    this.itemMenu.setRemoveBody();
-
-    this.body = new ImportTemplate(path.resolve(__dirname, './view.html'));
-    this.body.on('load', element => this._onLoad(element));
+    super('domoto-mia-cucina', templatePath, StoveController);
   }
 
-  _onLoad() {
-    this.controller = new StoveController(this.body, this.itemMenu);
-    this.emit('ready', this);
+  get _itemMenuDescription() {
+    return 'Mia cucina';
   }
 };
