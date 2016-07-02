0c74c456c469f5185b4ec87fc2a418b0588fcb0b
José Lobo <jloboprs@gmail.com>
2016-07-02 01:54:36 -0500

    Se renombran las clases para ser mas descriptibles

--- domotoMiaCucina.js
+++ domotoMiaCucina.js
@@ -4,22 +4,22 @@ const ItemMenu = require('domoto/itemMenu');
 const EventEmitter = require('events');
 const path = require('path');
 
-module.exports = class installView extends EventEmitter {
+module.exports = class DomotoMiaCucina extends EventEmitter {
   constructor() {
     super();
 
     this.controller = null;
-
-    this.itemMenu = new ItemMenu('domoto-mia-cucina');
+    this.name = 'domoto-mia-cucina';
+    this.itemMenu = new ItemMenu(this.name);
     this.itemMenu.description = 'Mia cucina';
     this.itemMenu.addLeftIcon('power_settings_new');
     this.itemMenu.setRemoveBody();
 
     this.body = new ImportTemplate(path.resolve(__dirname, './view.html'));
-    this.body.on('load', element => this._init(element));
+    this.body.on('load', element => this._onLoad(element));
   }
 
-  _init() {
+  _onLoad() {
     this.controller = new StoveController(this.body, this.itemMenu);
     this.emit('ready', this);
   }
