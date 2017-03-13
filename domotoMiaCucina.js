142b01751f5393171507928b9d51e0ff1b339bd6
José Lobo <jloboprs@gmail.com>
2017-03-12 19:30:46 -0500

    Se ajusta la composición de la extensión al nuevo cambio

--- domotoMiaCucina.js
+++ domotoMiaCucina.js
@@ -1,13 +1,13 @@
 const Domoto = require('domoto');
 const StoveController = require('./stoveController');
-const templatePath = require('path').resolve(__dirname, './view.html');
+const viewPath = require('path').resolve(__dirname, './view.html');
 
 module.exports = class DomotoMiaCucina extends Domoto {
   constructor() {
-    super('domoto-mia-cucina', templatePath, StoveController);
-  }
+    super('domoto-mia-cucina');
 
-  get _itemMenuDescription() {
-    return 'Mia cucina';
+    this.description = 'Mia cucina';
+    const view = this.addView(viewPath, StoveController, this.itemMenu);
+    this.itemMenu.on('click', this.viewManager.show(view));
   }
 };
